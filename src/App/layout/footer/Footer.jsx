3c181dea8df39c0690b9faabdeb19bb9ed97f79f
AgusCFx <agusfx00@gmail.com>
2022-08-09 14:53:49 -0300

    mod main, add footer, add images

---
+++ src/App/layout/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import './footer.css'
+
+export const Footer = () => {
+  return (
+    <footer>
+        <h3 className='footer__contact'>
+            Horario: Lunes a Viernes 9 a 15hs<br />
+            Telefono: 341 468-8840<br />
+            Llamadas y Whatsapp<br />
+        </h3>
+        <h3 className='footer__ubication'>
+            Rosario, Santa Fe
+        </h3>
+    </footer>
+  )
+}
--- src/App/layout/main/Main.jsx
+++ src/App/layout/main/Main.jsx
@@ -1,6 +1,9 @@
 import { TextBox } from "../../../components/TextBox/TextBox"
 import { ImgBox } from "../../../components/ImgBox/ImgBox"
+import './main.css'
+
 import RepartoImg from "../../../assets/images/reparto-desk.jpg"
+import CatalogoImg from "../../../assets/images/catalogo-desk.jpg"
 
 export const Main = () => {
   return (
@@ -8,7 +11,13 @@ export const Main = () => {
         <TextBox textTitle='Recapado para Camiones' textDesc='Servicio de reparacion, recapado, recauchutado, reconstruccion, rellenado... damos nueva vida a tus neumaticos para que siga manteniendo el rendimiento más optimo posible.'/>
         <ImgBox setImg={RepartoImg} />
         <TextBox textDesc='Amplia gama de bandas especificas para distintas medidas, podes elegir del catalogo el que más se adapta a tus necesidades.'/>
-        <ImgBox setImg={RepartoImg} />
+        <ImgBox setImg={CatalogoImg} />
+        <button className='catalogoBtn'>
+          <div class="downloading">
+            <span class="custom-arrow"></span>
+          </div>
+          DESCARGAR CATALOGO
+        </button>
     </main>
   )
 }
